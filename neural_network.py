2ab7140fc95189c1eb7650f0fa37a8a67779d343
Joseph Solomon <elhakimjosephsolomon@gmail.com>
2023-02-24 22:03:31 +0000

    NN

--- neural_network.py
+++ neural_network.py
@@ -2,6 +2,18 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
-class AirbnbNightlyPriceImageDataset(Dataset):
-    def __init__(self, csv_file):
-        self.data = pd.read_csv(csv_file)
+from torch.utils.data import DataLoader, SubsetRandomSampler
+
+dataset = AirbnbNightlyPriceImageDataset("airbnb_data.csv")
+dataset_size = len(dataset)
+indices = list(range(dataset_size))
+split = int(dataset_size * 0.8)
+
+train_indices, val_indices = indices[:split], indices[split:]
+
+train_sampler = SubsetRandomSampler(train_indices)
+val_sampler = SubsetRandomSampler(val_indices)
+
+train_loader = DataLoader(dataset, batch_size=64, sampler=train_sampler)
+val_loader = DataLoader(dataset, batch_size=64, sampler=val_sampler)
+test_loader = DataLoader(dataset, batch_size=64, shuffle=True, sampler=None)
